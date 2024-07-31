eb6e6a208e1a2a95310ef5e756c243880285284c
Davide Altamura <davide.altamura97@gmail.com>
2024-07-31 21:37:42 +0200

    Add Character Controller & Crosshair

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public Camera playerCamera;
+    private Vector3 moveDirection;
+    private float rotationX;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float rotationSpeed;
+    [SerializeField] private float maxLookUpAngle = 90f;
+    [SerializeField] private float maxLookDownAngle = -90f;
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 right = transform.TransformDirection(Vector3.right);
+
+        moveDirection = (forward * Input.GetAxis("Vertical")) + (right * Input.GetAxis("Horizontal"));
+
+        controller.Move(moveDirection * Time.deltaTime * moveSpeed);
+        float mouseY = Input.GetAxis("Mouse Y") * rotationSpeed;
+        float mouseX = Input.GetAxis("Mouse X") * rotationSpeed;
+        rotationX -= mouseY;
+        rotationX = Mathf.Clamp(rotationX, maxLookDownAngle, maxLookUpAngle);
+        playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+        transform.rotation *= Quaternion.Euler(0, mouseX * rotationSpeed, 0);
+    }
+}
+
